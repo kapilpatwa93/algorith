938cb21871e5485271ec5aa726c5985fe7cae9d4
kapilpatwa93 <work.kapilp@gmail.com>
2020-08-23 20:02:38 +0530

    leetcode-distribute_candies_to_people

---
+++ leetcode/distribute_candies_to_people.go
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/distribute-candies-to-people/
+// 1103. Distribute Candies to People
+package main
+
+import "fmt"
+
+func distributeCandies(candies int, numPeople int) []int {
+	arr := make([]int, numPeople)
+	round := 0
+	i := 0
+	for {
+		if i == numPeople {
+			round++
+			i = 0
+		}
+		c := round*numPeople + i + 1
+		if c > candies {
+			c = candies
+		}
+		arr[i] += c
+		candies -= c
+		if candies <= 0 {
+			break
+		}
+		i++
+	}
+	return arr
+
+}
+func main() {
+	candies := 7
+	numPeople := 4
+	res := distributeCandies(candies, numPeople)
+	fmt.Println(res)
+}
