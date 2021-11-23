c39ab0509dce683381f8859c3037b553247e9724
kapilpatwa93 <work.kapilp@gmail.com>
2021-11-23 22:06:12 +0530

    leetcode-binary_search.py

---
+++ leetcode/binary_search.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+        index = -1
+        while start <= end:
+            mid = int((start + end) / 2)
+            print(mid, start, end)
+            if nums[mid] == target:
+                return mid
+            if target < nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return index
+
+
+def main():
+    nums = [-1, 0, 3, 5, 9, 12]
+
+    target = 9
+    res = Solution().search(nums, target)
+    print(res)
+
+
+main()
