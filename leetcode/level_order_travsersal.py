72063639b77eefdeb1c7b6257266a79411cd7458
kapilpatwa93 <work.kapilp@gmail.com>
2021-11-30 13:43:53 +0530

    leetcode-level_order_traversal(non-recursive)

--- leetcode/level_order_travsersal.py
+++ leetcode/level_order_travsersal.py
@@ -11,6 +11,32 @@ class TreeNode:
 
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        return self.nonRecursive(root)
+
+    def nonRecursive(self, root: Optional[TreeNode]) -> List[List[int]]:
+        queue = []
+        if root is None:
+            return queue
+        queue = [[root]]
+        levels = []
+        index = 0
+        while index < len(queue):
+            level = []
+            q = []
+            for node in queue[index]:
+                if node is not None:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if len(q):
+                queue.append(q)
+            if len(level):
+                levels.append(level)
+            index += 1
+
+        return levels
+
+    def recursive(self, root: Optional[TreeNode]) -> List[List[int]]:
         levels = []
 
         def recurse(subroot: Optional[TreeNode], level):
@@ -46,8 +72,8 @@ def getTree(nodes: List[int]) -> TreeNode:
 
 
 if __name__ == '__main__':
-    nodes = [3, 9, 20, None, None, 15, 7]
-    # nodes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+    # nodes = [3, 9, 20, None, None, 15, 7]
+    nodes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
     tree = getTree(nodes)
     res = Solution().levelOrder(tree)
     print(res)
