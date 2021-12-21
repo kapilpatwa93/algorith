cde6078f35e7d6c65930937886cb6dfa34c77ba6
kapilpatwa93 <work.kapilp@gmail.com>
2021-12-21 13:52:11 +0530

    leetcode-combinations

---
+++ leetcode/combinations.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+class Solution:
+    list = []
+
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        self.list = []
+        self.combinations(n, 0, k, [])
+        return self.list
+
+    def combinations(self, n, curr, k, arr: List[int]):
+        if len(arr) == k:
+            self.list.append(arr[:])
+            return
+        index = curr
+        while index < n:
+            a = arr[:]
+            a.append(index + 1)
+            self.combinations(n, index + 1, k, a)
+            index += 1
+        return
+
+
+if __name__ == '__main__':
+    n = 4
+    k = 2
+    res = Solution().combine(n, k)
+    print(res)
