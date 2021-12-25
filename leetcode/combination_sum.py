514a424d20ee7744287adb2efcd77d9e59c0c58a
kapilpatwa93 <work.kapilp@gmail.com>
2021-12-25 15:17:07 +0530

    leetcode-combination_sum

---
+++ leetcode/combination_sum.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+
+        def findSum(arr: List[int], sum: int, currIndex: int):
+            if sum == target:
+                result.append(arr)
+                return
+
+            if sum > target:
+                return
+
+            for index in range(currIndex, (len(candidates))):
+                num = candidates[index]
+                a = arr[:]
+                a.append(num)
+                findSum(a, sum + num, index)
+
+        findSum([], 0, 0)
+        return result
+
+
+if __name__ == '__main__':
+    candidates = [5, 2,4,3]
+    target = 8
+    res = Solution().combinationSum(candidates, target)
+    print(res)
