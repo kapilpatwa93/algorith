24ab150b27bbf6d7473a02551f865ebbe252b6ad
kapilpatwa93 <work.kapilp@gmail.com>
2021-12-29 13:15:01 +0530

    leetcode-unique_morse_code_words

---
+++ leetcode/unique_morse_code_words.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+from typing import List
+
+codes = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+         ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."]
+
+
+def getMorseForAChar(c: str) -> str:
+    return codes[ord(c) - 97]
+
+
+def getMorseForStr(string: str) -> str:
+    morse = ""
+    for c in string:
+        morse += getMorseForAChar(c)
+    return morse
+
+
+class Solution:
+    def uniqueMorseRepresentations(self, words: List[str]) -> int:
+        morseMap = defaultdict(lambda: 0)
+        for w in words:
+            morseMap[getMorseForStr(w)] += 1
+        return len(morseMap)
+
+
+if __name__ == '__main__':
+    words = ["gin", "zen", "gig", "msg"]
+    res = Solution().uniqueMorseRepresentations(words)
+    print(res)
