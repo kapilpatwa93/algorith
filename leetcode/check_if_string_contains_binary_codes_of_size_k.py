952223d0daf7216494aa35b67077bdeb551dbd6d
kapilpatwa93 <work.kapilp@gmail.com>
2022-05-31 18:52:27 +0530

    leetcode-check_if_string_contains_binary_codes_of_size_k

---
+++ leetcode/check_if_string_contains_binary_codes_of_size_k.py
@@ -0,0 +1,22 @@
+import math
+
+
+class Solution:
+
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        m = dict()
+        for i in range(0, len(s) - k + 1):
+            m[s[i:i + k]] = 1
+        has = True
+        for i in range(int(math.pow(2, k))):
+            key = format(i, "b").rjust(k, "0")
+            if not key in m:
+                has = False
+        return has
+
+
+if __name__ == '__main__':
+    s = "111011001000"
+    k = 3
+    res = Solution().hasAllCodes(s, k)
+    print(res)
