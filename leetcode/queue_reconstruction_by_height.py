f639ed8d0b6b6f4c8e64af9cb03aaeb737d6d327
kapilpatwa93 <work.kapilp@gmail.com>
2022-06-29 19:10:44 +0530

    leetcode-queue_reconstruction_by_height

--- leetcode/queue_reconstruction_by_height.py
+++ leetcode/queue_reconstruction_by_height.py
@@ -1,30 +1,19 @@
-import functools
 from typing import List
 
 
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        people.sort(key=lambda x: (-x[0], x[1]))
+        i = 0
 
-        def moveAndShift(current: int, moveTo: int):
-            for i in reversed(range(moveTo + 1, current + 1)):
-                people[i], people[i - 1] = people[i - 1], people[i]
+        def moveAndShift(current: int, desired: int):
+            c = people.pop(current)
+            people.insert(desired, c)
 
-        def compare(key1, key2):
-            if key1[0] > key2[0]:
-                return 1
-            elif key1[0] < key2[0]:
-                return -1
-            else:
-                if key1[1] > key2[1]:
-                    return -1
-                elif key1[1] < key2[1]:
-                    return 1
-                return 0
-
-        people.sort(key=functools.cmp_to_key(compare), reverse=True)
-        for i in range(len(people)):
-            if i > people[i][1]:
-                moveAndShift(i, people[i][1])
+        while i < len(people):
+            h = people[i]
+            moveAndShift(i, h[1])
+            i += 1
         return people
 
 
