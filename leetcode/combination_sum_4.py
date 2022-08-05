51663e62eb83e1b36acdba9a0535045bb13f0ef4
kapilpatwa93 <work.kapilp@gmail.com>
2022-08-05 13:12:56 +0530

    leetcode-combination_sum_4

---
+++ leetcode/combination_sum_4.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        m = {}
+
+        def recurse(target: int):
+            if target < target:
+                return 0
+            if target == 0:
+                return 1
+            if target in m:
+                return m[target]
+            sum = 0
+            for n in nums:
+
+                if target >= n:
+                    sum += recurse(target - n)
+
+            m[target] = sum
+            return sum
+
+        return recurse(target)
+
+
+if __name__ == '__main__':
+    nums = [1, 2, 3]
+    target = 4
+    res = Solution().combinationSum4(nums, target)
+    print(res)
