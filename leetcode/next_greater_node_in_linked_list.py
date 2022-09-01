8f05baea2544246de238ff610cf99bedb30f510c
kapilpatwa93 <work.kapilp@gmail.com>
2022-09-01 20:46:21 +0530

    leetcode-next_greater_node_in_linked_list

--- leetcode/next_greater_node_in_linked_list.py
+++ leetcode/next_greater_node_in_linked_list.py
@@ -15,17 +15,11 @@ class Solution:
         h = head
         i = 0
 
-        def removeFromStack(v: (int, int)):
-            while len(stack) > 0 and v[0] > stack[-1][0]:
-                popped = stack.pop()
-                ans[popped[1]] = v[0]
-            stack.append(v)
-
         while h is not None:
-            if len(stack) > 0 and h.val < stack[-1][0]:
-                stack.append((h.val, i))
-            else:
-                removeFromStack((h.val, i))
+            while len(stack) > 0 and h.val > stack[-1][0]:
+                popped = stack.pop()
+                ans[popped[1]] = h.val
+            stack.append((h.val, i))
             h = h.next
             i += 1
             ans.append(0)
@@ -44,7 +38,7 @@ def constructLinkedList(list: List[int]) -> Optional[ListNode]:
 
 
 if __name__ == '__main__':
-    list = [1,2,3,4,5,4,3,2,1,1,2,3,4,6,45,3]
+    list = [1, 2, 3, 4, 5, 4, 3, 2, 1, 1, 2, 3, 4, 6, 45, 3]
     n = 2
     ll = constructLinkedList(list)
     res = Solution().nextLargerNodes(ll)
