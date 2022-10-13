cf1e56c345f5ecd447ea102955561dd561f660c5
kapilpatwa93 <work.kapilp@gmail.com>
2022-10-13 17:04:52 +0530

    leetcode-convert_sorted_array_to_binary_search_tree

--- leetcode/convert_sorted_array_to_binary_search_tree.py
+++ leetcode/convert_sorted_array_to_binary_search_tree.py
@@ -24,6 +24,6 @@ class Solution:
 
 
 if __name__ == '__main__':
-    nums = [3, 5, 7, 10, 12, 15, 20]
+    nums = [3, 5, 7, 10, 12, 15, 20, 25]
     res = Solution().sortedArrayToBST(nums)
     print(res)
